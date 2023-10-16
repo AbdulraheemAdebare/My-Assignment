695d2a0cb4cb726c935e69d9a3d9698951ae32fa
Abdulraheem Adebare <abdulraheemadebare55@gmail.com>
2023-10-16 18:23:48 +0100

    This is the first chapter of my Assignment

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Number 5
+using System.Globalization;
+
+Console.WriteLine("Good Morning");
+
+// Number 6
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine()!;
+Console.Write("Enter your second name: ");
+string lname = Console.ReadLine()!;
+
+// Number 7
+Console.WriteLine("1\n101\n1001");
+
+// Number 8
+Console.WriteLine(DateTime.Now);
+
+// Number 9
+Console.WriteLine(Math.Sqrt(12345));
+
+// Number 10
+int loop = 2;
+for(loop = 2; loop <= 100; loop++)
+{
+    Console.WriteLine(loop);
+}
+
+// Number 11
+Console.WriteLine("What is your age? ");
+int yourAge = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"You will be {yourAge + 10} in the next ten years.");
+
+// Number 12
+
+// Number 13
